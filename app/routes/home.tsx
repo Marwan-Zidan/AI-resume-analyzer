b6d0950fc02d55a6002f56860b2da0876eec28e0
Marwan Zidan <marwanahmedd256@gmail.com>
2026-04-05 19:03:22 +0200

    add ReasumeCard component ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
+import { resumes } from "../../constants/index";
 import Navbar from "../components/Navbar";
 import type { Route } from "./+types/home";
+import ResumeCard from "../components/ResumeCard";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Reazume Analyzer" },
@@ -12,10 +14,17 @@ export default function Home() {
 
 <Navbar/>
 <section className="main-section">  
-<div className="page-heading">
+<div className="page-heading  py-16">
   <h1>Track your application & Resume Rating</h1>
   <h2>Review your submissions and check AI-powered feedback. </h2>
 </div>
+
+{resumes.length>0 &&(
+  <div className="resumes-section">
+{resumes.map((resume) => (
+  <ResumeCard key={resume.id} resume={resume} />
+))}
+</div>)}
 </section>
   </main>
   );
