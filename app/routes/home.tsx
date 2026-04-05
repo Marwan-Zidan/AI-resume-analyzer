97a7906876e3da7e2efb14f5cf5d1848a5bc94fd
Marwan Zidan <marwanahmedd256@gmail.com>
2026-04-05 17:42:21 +0200

    add navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -8,9 +8,9 @@ export function meta({}: Route.MetaArgs) {
 }
 export default function Home() {
   return (
-    
   <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
+
+<Navbar/>
 <section className="main-section">  
 <div className="page-heading">
   <h1>Track your application & Resume Rating</h1>
