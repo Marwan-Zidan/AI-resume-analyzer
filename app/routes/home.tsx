438f205e76126f49de89a302964a5c929c28125f
Marwan Zidan <marwanahmedd256@gmail.com>
2026-04-06 21:36:22 +0200

    Add uplode tsx file route ,get  linked and customize the UI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@ import { resumes } from "../../constants/index";
 import Navbar from "../components/Navbar";
 import type { Route } from "./+types/home";
 import ResumeCard from "../components/ResumeCard";
-import { usePuterStore } from "~/lib/Puter"
+import { usePuterStore } from "~/lib/Puter";
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
 export function meta({}: Route.MetaArgs) {
@@ -15,25 +15,25 @@ export default function Home() {
   const { auth } = usePuterStore();
   const navigate = useNavigate();
   useEffect(() => {
-    if(!auth.isAuthenticated) navigate('/auth?next=/');
+    if (!auth.isAuthenticated) navigate("/auth?next=/");
   }, [auth.isAuthenticated]);
   return (
-  <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading  py-16">
+          <h1>Track your application & Resume Rating</h1>
+          <h2>Review your submissions and check AI-powered feedback. </h2>
+        </div>
 
-<Navbar/>
-<section className="main-section">  
-<div className="page-heading  py-16">
-  <h1>Track your application & Resume Rating</h1>
-  <h2>Review your submissions and check AI-powered feedback. </h2>
-</div>
-
-{resumes.length>0 &&(
-  <div className="resumes-section">
-{resumes.map((resume) => (
-  <ResumeCard key={resume.id} resume={resume} />
-))}
-</div>)}
-</section>
-  </main>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
   );
-};
+}
---
+++ app/routes/upload.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+const upload = () => {
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading  py-16">
+          <h1>Smart feedback for your dream job</h1>
+          <h2>Get AI-powered feedback to boost your job applications. </h2>
+        </div>
+      </section>
+    </main>
+  );
+};
+export default upload;
