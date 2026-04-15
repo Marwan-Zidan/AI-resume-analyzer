2e0465d7d4bcd7b418a557ea1fa672359b7cefb0
Marwan Zidan <marwanahmedd256@gmail.com>
2026-04-15 11:07:48 +0200

    Add Details component

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -73,7 +73,8 @@ const Resume = () => {
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                         <Summary feedback={feedback} />
-                        <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
+                        <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} /> 
+                        <Details feedback={feedback} />
                         </div>
                     ) : (
                         <img src="../../public/images/resume-scan-2.gif" className="w-full" />
