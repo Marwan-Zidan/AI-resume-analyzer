e8e0db3a921ce014caa9b3a2bf2b925bb510fc49
Marwan Zidan <marwanahmedd256@gmail.com>
2026-04-15 07:34:12 +0200

    desturcture id using params from react router  and add meta data

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { useParams } from 'react-router';
 
+
+export const meta = () => [
+  { title: "Resumind | Review" },
+  { name: "description", content: "Detailed overview of your resume" },
+];
 const resume = () => {
+  const { id } = useParams();
   return (
     <div>
-      
+          <h1>Resume  {id}</h1>
     </div>
   )
 }
