d2518adcea9bf9cbb71bc1b0ef0a2351a493f08f
Marwan Zidan <marwanahmedd256@gmail.com>
2026-04-19 04:11:20 +0200

    Add ScoreGuage  component

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import ScoreGuage from './ScoreGauge';
 const Summary = () => {
   return (
-    <div>
-      Summary
+    <div className="bg-white w-full rounded-2xl shadow-md  " >
+      <div className='flex flex-col items-center  p-4 gap-8'>
+        <h2 className='text-3xl font-bold text-gray-800'>Summary</h2>
+        <ScoreGuage  />
+      </div>
     </div>
   )
 }
