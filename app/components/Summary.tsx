25ab8a4a3fe64703334c61f61632f9c0ebc097cb
Marwan Zidan <marwanahmedd256@gmail.com>
2026-04-19 04:15:02 +0200

    Add UI to ScoreGauge  component

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ScoreGuage from './ScoreGauge';
-const Summary = () => {
+import ScoreGauge from './ScoreGauge';
+const Summary = ({feedback}: {feedback: Feedback}) => {
   return (
     <div className="bg-white w-full rounded-2xl shadow-md  " >
       <div className='flex flex-col items-center  p-4 gap-8'>
         <h2 className='text-3xl font-bold text-gray-800'>Summary</h2>
-        <ScoreGuage  />
+        <ScoreGauge score={feedback.overallScore} />
       </div>
     </div>
   )
